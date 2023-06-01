7650bb214299af308e2352f789a68e24478d51da
ARIF-SUHAIMI <arif.suhaimijb@gmail.com>
2023-06-01 10:19:38 +0800

    Update user settings function Add new function to update the User table Remove unused tabs in userSettings.php add new function to update password load all user related stuff into userSettings.php
    Signed-off-by: ARIF-SUHAIMI <arif.suhaimijb@gmail.com>

---
+++ PHP/updateUser.php
@@ -0,0 +1,28 @@
+<?php
+
+    session_start();
+    require("config.php");
+
+    if (!$conn) {
+        echo 'failure';
+        die('Connection failed: ' . mysqli_connect_error());
+    }
+
+    $id = $_SESSION["ID"];$name = $_POST["name"];
+    $lname = $_POST["lname"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $position = $_POST["position"];
+
+    $stmt = $conn->prepare("UPDATE Users 
+    SET name = ?, Last_Name = ?, email = ?, phone = ?,position = ? WHERE id = ?");
+    $stmt->bind_param("sssssi", $name, $lname, $email, $phone,$position,$id);
+
+    if ($stmt->execute()) {
+        // Update successful
+        echo "success";
+    } else {
+        // Update failed
+        echo "failure";
+    }
+?>
--- PHP/userSettings.php
+++ PHP/userSettings.php
@@ -9,6 +9,41 @@ function displayCurrentDateTime()
     $currentDateTime = date('Y-m-d');
     echo "Current date: $currentDateTime";
 }
+
+require("config.php");
+
+if (!$conn) {
+    echo 'failure';
+    die('Connection failed: ' . mysqli_connect_error());
+}
+
+$id = $_SESSION["ID"];
+
+$strsql = "SELECT * FROM Users Where id = $id";
+echo $strsql;
+$result = mysqli_query($conn, $strsql);
+$rows = mysqli_fetch_assoc($result);
+
+if ($result) {
+    if (mysqli_num_rows($result) == 1) {
+        mysqli_close($conn);
+        ob_clean();
+        $name = $rows["name"];
+        $email = $rows["email"];
+        $password = $rows["password"];
+        $phone = $rows["phone"];
+        $lname = $rows["Last_Name"];
+        $pos = $rows["position"];
+        $fullname = $name . " " . $lname;
+    } else {
+        mysqli_close($conn);
+        ob_clean();
+        echo "fail";
+    }
+} else {
+    mysqli_close($conn);
+    die('Query failed: ' . mysqli_error($conn));
+}
 ?>
 
 <!DOCTYPE html>
@@ -22,7 +57,7 @@ function displayCurrentDateTime()
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>FYP-CSMS -Charts</title>
+    <title>FYP-CSMS - Charts</title>
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -31,6 +66,7 @@ function displayCurrentDateTime()
 
     <!-- Custom styles for this template-->
     <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../CSS/userSettings.css" rel="stylesheet">
 
 </head>
 
@@ -129,10 +165,16 @@ function displayCurrentDateTime()
             </li>
 
             <!-- Nav Item - Charts -->
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href="../PHP/charts.php">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Charts</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="../PHP/Sales.php">
+                    <i class="fas fa-shopping-cart"></i>
+                    <span>Sales</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
@@ -316,7 +358,7 @@ function displayCurrentDateTime()
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $fullname  ?></span>
                                 <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
                             </a>
                             <!-- Dropdown - User Information -->
@@ -358,7 +400,7 @@ function displayCurrentDateTime()
                                             <div class="img-circle text-center mb-3">
                                                 <img src="img/user2.jpg" alt="Image" class="shadow">
                                             </div>
-                                            <h4 class="text-center">Kiran Acharya</h4>
+                                            <h4 class="text-center"><?php echo $name ?></h4>
                                         </div>
                                         <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                                             <a class="nav-link active" id="account-tab" data-toggle="pill" href="#account" role="tab" aria-controls="account" aria-selected="true">
@@ -369,7 +411,7 @@ function displayCurrentDateTime()
                                                 <i class="fa fa-key text-center mr-1"></i>
                                                 Password
                                             </a>
-                                            <a class="nav-link" id="security-tab" data-toggle="pill" href="#security" role="tab" aria-controls="security" aria-selected="false">
+                                            <!-- <a class="nav-link" id="security-tab" data-toggle="pill" href="#security" role="tab" aria-controls="security" aria-selected="false">
                                                 <i class="fa fa-user text-center mr-1"></i>
                                                 Security
                                             </a>
@@ -380,89 +422,93 @@ function displayCurrentDateTime()
                                             <a class="nav-link" id="notification-tab" data-toggle="pill" href="#notification" role="tab" aria-controls="notification" aria-selected="false">
                                                 <i class="fa fa-bell text-center mr-1"></i>
                                                 Notification
-                                            </a>
+                                            </a> -->
                                         </div>
                                     </div>
                                     <div class="tab-content p-4 p-md-5" id="v-pills-tabContent">
                                         <div class="tab-pane fade show active" id="account" role="tabpanel" aria-labelledby="account-tab">
                                             <h3 class="mb-4">Account Settings</h3>
                                             <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>First Name</label>
-                                                        <input type="text" class="form-control" value="Kiran">
+                                                <form action="../PHP/updateUser.php" method="POST" id="userForm">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>First Name</label>
+                                                            <input type="text" class="form-control" value=<?php echo $name ?> name="name">
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>Last Name</label>
-                                                        <input type="text" class="form-control" value="Acharya">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Last Name</label>
+                                                            <input type="text" class="form-control" value=<?php echo $lname ?> name="lname">
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>Email</label>
-                                                        <input type="text" class="form-control" value="kiranacharya287@gmail.com">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Email</label>
+                                                            <input type="text" class="form-control" value=<?php echo $email ?> name="email">
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>Phone number</label>
-                                                        <input type="text" class="form-control" value="+91 9876543215">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Phone number</label>
+                                                            <input type="text" class="form-control" value=<?php echo $phone ?> name="phone">
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>Company</label>
-                                                        <input type="text" class="form-control" value="Kiran Workspace">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Company</label>
+                                                            <input type="text" class="form-control" value="Universiti Teknologi Malaysia" readonly>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>Designation</label>
-                                                        <input type="text" class="form-control" value="UI Developer">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Position</label>
+                                                            <input type="text" class="form-control" value=<?php echo $pos ?> name="position">
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <label>Bio</label>
-                                                        <textarea class="form-control" rows="4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore vero enim error similique quia numquam ullam corporis officia odio repellendus aperiam consequatur laudantium porro voluptatibus, itaque laboriosam veritatis voluptatum distinctio!</textarea>
+                                                    <div class="col-md-12">
+                                                        <div class="form-group">
+                                                            <label>Bio</label>
+                                                            <textarea class="form-control" rows="4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore vero enim error similique quia numquam ullam corporis officia odio repellendus aperiam consequatur laudantium porro voluptatibus, itaque laboriosam veritatis voluptatum distinctio!</textarea>
+                                                        </div>
                                                     </div>
-                                                </div>
                                             </div>
                                             <div>
-                                                <button class="btn btn-primary">Update</button>
+                                                <input type="submit" class="btn btn-primary" value="Update"></input>
                                                 <button class="btn btn-light">Cancel</button>
                                             </div>
+                                            </form>
                                         </div>
                                         <div class="tab-pane fade" id="password" role="tabpanel" aria-labelledby="password-tab">
                                             <h3 class="mb-4">Password Settings</h3>
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>Old password</label>
-                                                        <input type="password" class="form-control">
+                                            <form action="../PHP/updatePassword.php" method="POST" id="userPass">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Old password</label>
+                                                            <input type="password" class="form-control" name="oldPass">
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>New password</label>
-                                                        <input type="password" class="form-control">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>New password</label>
+                                                            <input type="password" class="form-control" name="newPass">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Confirm new password</label>
+                                                            <input type="password" class="form-control" name="confPass">
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label>Confirm new password</label>
-                                                        <input type="password" class="form-control">
-                                                    </div>
+                                                <div>
+                                                    <input type="submit" class="btn btn-primary" value="Update"></input>
+                                                    <button class="btn btn-light">Cancel</button>
                                                 </div>
-                                            </div>
-                                            <div>
-                                                <button class="btn btn-primary">Update</button>
-                                                <button class="btn btn-light">Cancel</button>
-                                            </div>
+                                            </form>
                                         </div>
                                         <div class="tab-pane fade" id="security" role="tabpanel" aria-labelledby="security-tab">
                                             <h3 class="mb-4">Security Settings</h3>
@@ -614,6 +660,7 @@ function displayCurrentDateTime()
 
     <!-- Page level custom scripts -->
     <script src="../JS/logout.js"></script>
+    <script src="../JS/userSettings.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.4/sweetalert2.min.js"></script>
 </body>
